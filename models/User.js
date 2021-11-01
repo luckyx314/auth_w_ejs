61d2d81288e482059bd1264e074c275495640714
Lucky Jones Uayan <luckyjonesbuayan@gmail.com>
2021-11-02 06:34:40 +0800

    Mongoose Hooks Intro
    Firing a function before or after saving a doc to the db

--- models/User.js
+++ models/User.js
@@ -16,6 +16,19 @@ const UserSchema = new mongoose.Schema({
     },
 });
 
+// Mongoose Hooks
+// fire a function after doc (new user) saved to db
+UserSchema.post("save", function(doc, next) {
+    console.log("New user was created and saved", doc)
+    next(); // important - always put this
+})
+
+// fire a function before a doc (new user) saved to db
+UserSchema.pre("save", function(next) {
+    console.log("User about to be created and saved", this)
+    next()
+})
+
 const User = mongoose.model("user", UserSchema);
 
 module.exports = User;
