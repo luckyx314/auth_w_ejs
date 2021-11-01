84f9b71fde1088c0faf7d1cbe6f0a7b30b156cef
Lucky Jones Uayan <luckyjonesbuayan@gmail.com>
2021-11-02 05:42:31 +0800

    Mongoose User model initialized

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,23 +1,39 @@
+const User = require("../models/User");
+
 exports.signup_get = (req, res, next) => {
     res.render("signup");
-};
-
-exports.signup_post = (req, res, next) => {
-    const {email, password} = req.body;
-    res.status(201).json({
-        success: true,
-        data: "Signup complete"
-    })
 };
 
 exports.login_get = (req, res, next) => {
     res.render("login");
 };
 
-exports.login_post = (req, res, next) => {
-    const {email, password} = req.body;
+exports.signup_post = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.create({
+            email,
+            password,
+        });
+        res.status(201).json({
+            success: true,
+            msg: "User created",
+            data: user
+        });
+    } catch (error) {
+        console.log(error.code)
+        res.status(400).json({
+            success: false,
+            msg: "Error: User not created."
+        })
+    }
+};
+
+exports.login_post = async (req, res, next) => {
+    const { email, password } = req.body;
     res.status(201).json({
         success: true,
-        data: "Login complete"
-    })
+        data: "Login complete",
+    });
 };
