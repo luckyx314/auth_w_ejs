20d2024c1156ac5683b4c29d861f104dfbc3e4b5
Lucky Jones Uayan <luckyjonesbuayan@gmail.com>
2021-11-04 19:51:52 +0800

    Cookie Basics

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 require("dotenv").config({ path: "./config.env" });
+const cookieParser = require("cookie-parser");
 
 const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
 
-// middleware 
+// middleware
 app.use(express.static("public"));
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set("view engine", "ejs");
@@ -24,7 +26,17 @@ mongoose
     .catch((err) => console.log(err));
 
 // routes
-app.get("/", (req, res) => res.render("home"))
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get("/", (req, res) => res.render("home"));
+app.get("/smoothies", (req, res) => res.render("smoothies"));
 
-app.use("/api/auth", require("./routes/authRoutes")) // auth routes
+app.use("/api/auth", require("./routes/authRoutes")); // auth routes
+
+// cookies
+app.get("/set-cookies", (req, res, next) => {
+    // res.setHeader('Set-Cookie', 'newUser=true');
+
+    res.cookie('newUser', false, {maxAge: 1000 *  60 * 60 * 24, secure: true, httpOnly: true}) // same thing as above
+    res.send("You got the cookie")
+});
+
+app.get("/read-cookies", (req, res, next) => {});
