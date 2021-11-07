056fad401ce6a6f477d5693abf7a34c1ea6d93b4
Lucky Jones Uayan <luckyjonesbuayan@gmail.com>
2021-11-07 15:00:36 +0800

    Can now login
    Can log in but the routes are not protected yet.

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,8 +3,16 @@ const jwt = require("jsonwebtoken");
 
 // handle errors
 const handleErrors = (err) => {
-    // console.log(err.message, err.code);
+    console.log(err.message, err.code);
     let errors = { email: "", password: "" };
+
+    //incorrect email
+    if (err.message === "incorrect email") {
+        errors.email = "that email is not registered";
+    }
+    if (err.message === "incorrect password") {
+        errors.password = "that password is incorrect";
+    }
 
     // duplicate error code
     if (err.code === 11000) {
@@ -48,7 +56,7 @@ exports.signup_post = async (req, res, next) => {
             password,
         });
         const token = createToken(user._id);
-        res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge * 1000})
+        res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
         res.status(201).json({
             success: true,
             msg: "User created",
@@ -65,8 +73,21 @@ exports.signup_post = async (req, res, next) => {
 
 exports.login_post = async (req, res, next) => {
     const { email, password } = req.body;
-    res.status(201).json({
-        success: true,
-        data: "Login complete",
-    });
+
+    try {
+        const user = await User.login(email, password);
+        const token = createToken(user._id);
+        res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+        res.status(200).json({
+            success: true,
+            msg: "User created",
+            user: user._id,
+        });
+    } catch (error) {
+        const errors = handleErrors(error);
+        res.status(400).json({
+            success: false,
+            errors,
+        });
+    }
 };
--- models/User.js
+++ models/User.js
@@ -19,12 +19,24 @@ const UserSchema = new mongoose.Schema({
 
 // Mongoose Hooks
 // fire a function before a doc (new user) saved to db
-UserSchema.pre("save", async function(next) {
+UserSchema.pre("save", async function (next) {
     // console.log("User about to be created and saved", this);
-    const salt = await bcrypt.genSalt();   
-    this.password = await bcrypt.hash(this.password, salt)
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
     next();
 });
+
+UserSchema.statics.login = async function (email, password) {
+    const user = await this.findOne({ email });
+    if (user) {
+        const auth = await bcrypt.compare(password, user.password);
+        if (auth) {
+            return user;
+        }
+        throw Error("incorrect password");
+    }
+    throw Error("incorrect email");
+};
 
 const User = mongoose.model("user", UserSchema);
 
